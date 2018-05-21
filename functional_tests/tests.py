a9100ba243e19155b05cf87ccb81d39b791e566e
razzlepdx <rwilson.or@gmail.com>
2018-05-21 16:34:59 -0700

    refactor FT to avoid voodoo sleeps, speed up testing

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,8 +1,10 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
 import time
 
+MAX_WAIT = 10
 
 class NewVisitorTest(LiveServerTestCase):
 
@@ -16,10 +18,18 @@ class NewVisitorTest(LiveServerTestCase):
 
         self.browser.quit()
 
-    def check_for_row_in_list_table(self, row_text):
-        movies_list = self.browser.find_element_by_id('id_movies_list')
-        rows = movies_list.find_elements_by_tag_name('li')
-        self.assertIn(row_text, [row.text for row in rows])
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:
+                movies_list = self.browser.find_element_by_id('id_movies_list')
+                rows = movies_list.find_elements_by_tag_name('li')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
 
     def test_can_start_a_movies_list_and_retrieve_it_later(self):
 
@@ -47,9 +57,8 @@ class NewVisitorTest(LiveServerTestCase):
         # "1: John Wick" as an item in the "My Favorite Movies list"
 
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
 
-        self.check_for_row_in_list_table('John Wick')
+        self.wait_for_row_in_list_table('John Wick')
 
         # There is still a text box inviting Jesse to add another movie.
         # They enter "Constantine" (Jesse may be into Keanu Reeves.)
@@ -57,13 +66,10 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox = self.browser.find_element_by_id('id_movie_title')
         inputbox.send_keys('Constantine')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
 
         # The page updates again, and now shows both movies on their list.
 
-        movies_list = self.browser.find_element_by_id('id_movies_list')
-        rows = movies_list.find_elements_by_tag_name('li')
-        self.assertIn('Constantine', [row.text for row in rows])
+        self.wait_for_row_in_list_table('Constantine')
 
         # Jesse wonders whether the site will remember their list.  Then
         # they see that the site has generated a unique URL for them --
