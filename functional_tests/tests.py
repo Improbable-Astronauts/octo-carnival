fc563eb4e694ee863801d3ded22a25bb6aec3ed3
Daniel Phillips <daniel.j.phillips@gmail.com>
2018-06-10 22:52:54 -0700

    use env vars for prod settings DEBUG, ALLOWED_HOSTS, SECRET_KEY. Also use env vars for OMDB_API_KEY

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import WebDriverException
 import time
+import os
 
 MAX_WAIT = 10
 
@@ -12,6 +13,9 @@ class NewVisitorTest(LiveServerTestCase):
         """ Defines setup operations for the NewVisitorTest class. """
 
         self.browser = webdriver.Firefox()
+        staging_server = os.environ.get('STAGING_SERVER')
+        if staging_server:
+            self.live_server_url = 'http://' + staging_server
 
     def tearDown(self):
         """ Defines tear-down operations for the NewVisitorTest class. """
