86d14ccae931a1637be8e6510872b54aa89ad0b5
Daniel Phillips <daniel.j.phillips@gmail.com>
2018-06-19 17:02:33 -0700

    config for postgress

--- moviemvp/settings.py
+++ moviemvp/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'moviemvp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'movielist',
+        'USER': 'movielist',
+        'PASSWORD': 'yLu96vk7myv3ftoJ',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
