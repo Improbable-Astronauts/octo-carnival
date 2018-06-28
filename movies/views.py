775ded55b14b798d6287e6ba60ac03d029925b89
JasonOnes <jasonr.jones14@gmail.com>
2018-06-27 17:39:33 -0700

    multidictvalue error caught, message displayed

--- movies/views.py
+++ movies/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import redirect, render
 from django.http import HttpResponse, Http404
+from django.utils.datastructures import MultiValueDictKeyError
+from django.contrib import messages
 
 import omdb
 
@@ -10,12 +12,19 @@ def index(request):
     """ Creates an HTTP response for requests made to the index route. """
 
     if request.method == 'POST':
-        movie_dict = omdb.imdbid(request.POST['imdb_id'])
-        #TODO fix this so that movie_dict is passed directly to Movie.objects.create()
-        Movie.objects.create(title=movie_dict['title'],year=movie_dict['year'],
-                        imdb_id=movie_dict['imdb_id'],runtime=movie_dict['runtime'],
-                        rated=movie_dict['rated'],)
-        return redirect('/')
+        try:
+            movie_dict = omdb.imdbid(request.POST['imdb_id'])
+            #TODO fix this so that movie_dict is passed directly to Movie.objects.create()
+            Movie.objects.create(title=movie_dict['title'],year=movie_dict['year'],
+                            imdb_id=movie_dict['imdb_id'],runtime=movie_dict['runtime'],
+                            rated=movie_dict['rated'],)
+            return redirect('/')
+        except MultiValueDictKeyError:
+            
+            messages.add_message(request, messages.WARNING, 'You didn\'t select a movie to save, look for another?')
+            return render(request, 'movies/search.html')
+           
+            
     movies = Movie.objects.all()
 
     return render(request, 'movies/index.html', {'movie_list': movies})
