8ba76f825406ea05a8215fb8143adaeb52689821
Daniel Phillips <daniel.j.phillips@gmail.com>
2018-07-31 12:48:07 -0700

    change url routes for search and detail to end in slash. Change tests to match. Change search template title to Movie Search from My Movies

--- movies/urls.py
+++ movies/urls.py
@@ -5,6 +5,6 @@ app_name = 'movies' # needed to use namespace in moviemvp urls
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('search', views.search, name='search'),
-    path('detail', views.detail, name='movie_detail')
+    path('search/', views.search, name='search'),
+    path('detail/', views.detail, name='movie_detail')
 ]
